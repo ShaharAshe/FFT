e19aa6f3b2ad30b604147bad37dbd7e6179cab7b
ShaharAshe <shaharas30@gmail.com>
2024-05-08 10:18:02 +0300

    Add thr option to find the unit root

--- FFT.py
+++ FFT.py
@@ -24,9 +24,28 @@ def FFT(FFT_vector:list, Z:int, unit_root:int, level:int=1, root:list=[], invers
     return [((temp_1[i%(len(FFT_vector)//2)] + ((temp_2[i%(len(FFT_vector)//2)] * final_result[0][i])))%Z) for i in range(len(FFT_vector))]
 
 
+def gcd(num_1:int, num_2:int)->int:
+    if num_1 == 0:
+        return 0
+    return num_1 if num_2 == 0 else gcd(num_2, num_1%num_2)
+
+
 if __name__ == "__main__":
     FFT_vector:list = [1,16,2,15,3,14,4,13,5,12,6,11,7,10,8,9]
     Z:int = 17
     unit_root:int = 6
 
-    print(FFT(FFT_vector, Z, unit_root))
+    "if you dont know the unit root, set it to 0"
+    # unit_root:int = 0
+
+    if not unit_root:
+        for i in range(2, Z):
+            if gcd(i, Z) == 1:
+                unit_root = i
+                break
+
+    if unit_root == 0:
+        print("No unit root found")
+    else:
+        print(f"Unit root: {unit_root}")
+        print(FFT(FFT_vector, Z, unit_root))
